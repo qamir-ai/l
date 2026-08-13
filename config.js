0c7628d7f54732fa60648a2736f35c3844de0677
QAMIR <abbosqayumov2003@gmail.com>
2026-08-13 07:41:39 +0500

    Delete config.js

--- config.js
+++
@@ -1,2 +0,0 @@
-/* API key shu yerda. Kerak bo‘lmasa bo‘sh qoldiring. OMMAVIY GitHub Pages'da key ko‘rinadi. */
-window.QAMIR_CONFIG={GEMINI_API_KEY:"AQ.Ab8RN6JpAXFVcJBGWeY9_GFgHWhAXubQqcAmmBZFSKKe8JPDOA",GEMINI_MODEL:"gemini-2.5-flash"};
